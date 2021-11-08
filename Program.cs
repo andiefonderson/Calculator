6d9f21621414ef4b84a7072af6542dc01b4e2311
Andie Fonderson <Andrea.Fonderson@IRIS.CO.UK>
2021-11-08 11:58:05 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the calculator! Here to calculate all your calculating needs. :)");
+        }
+    }
+}
