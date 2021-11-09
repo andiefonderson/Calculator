1520198ea732c9f4f0be2acc81c5f1bb79ac8df0
Andie Fonderson <Andrea.Fonderson@IRIS.CO.UK>
2021-11-09 18:37:03 +0000

    Added the date calculator.

--- Program.cs
+++ Program.cs
@@ -5,20 +5,47 @@ namespace Calculator
     class Program
     {
         static bool startCalculation = false;
+        const int NumberCalculatorNo = 1;
+        const int DateCalculatorNo = 2;
 
         static void Main(string[] args)
         {
             WelcomeMessage();
+
             while (!startCalculation)
             {
-                CalculateIfNoError();
-            }            
+                int calculatorMode = SelectMode();
+                if (calculatorMode == NumberCalculatorNo)
+                {
+                    NumbersCalculator();
+                }
+                else
+                {
+                    DatesCalculator();
+                }
+                RestartPrompt();
+            }
         }
 
         static void WelcomeMessage()
         {
-            Console.WriteLine("Welcome to the calculator! Here to calculate all your calculating needs. :)");
-            Console.WriteLine("Please make sure to do exactly as the instructions say so that you won't get an error.");
+            Console.WriteLine("Welcome to the calculator! Here to calculate all your calculating needs. :) " +
+                "\nPlease make sure to do exactly as the instructions say so that you won't get an error.");
+        }
+
+        static int SelectMode()
+        {
+            startCalculation = true;
+            Console.WriteLine("\nType in the number of the calculator you want to use:" +
+                "\n1) Number Calculator" +
+                "\n2) Date Calculator");
+            int numSelection = int.Parse(CheckIfValidNumber(Console.ReadLine()).ToString());
+            while (numSelection > 2 || numSelection < 1)
+            {
+                Console.WriteLine("Please only enter 1 for Number Calculator or 2 for Date Calculator.");
+                numSelection = int.Parse(Console.ReadLine());
+            }
+            return numSelection;
         }
 
         static string SelectOperator(string op)
@@ -38,9 +65,8 @@ namespace Calculator
             }
         }
 
-        static void CalculateIfNoError()
+        static void NumbersCalculator()
         {
-            startCalculation = true;
             Console.WriteLine("");
             string operatorToUse = "";
             string operatorText = "";
@@ -51,10 +77,21 @@ namespace Calculator
                 operatorToUse = Console.ReadLine();
                 operatorText = SelectOperator(operatorToUse);
             }
-            CalculateNumber(operatorText, operatorToUse);
+            Calculations(operatorText, operatorToUse);
         }
 
-        static void CalculateNumber(string opText, string op)
+        static void DatesCalculator()
+        {
+            Console.WriteLine("Please enter a date in DD/MM/YYYY or DD/MM/YY format:");
+            DateTime dateInput = CheckIfValidDate(Console.ReadLine());
+
+            Console.WriteLine("Please enter the number of days to add to the above-entered date.");
+            float daysAmount = CheckIfValidNumber(Console.ReadLine());
+
+            Console.WriteLine("It will be " + dateInput.AddDays(daysAmount).ToLongDateString() + ".");
+        }
+
+        static void Calculations(string opText, string op)
         {
             Console.WriteLine("How many numbers do you want to " + opText + "?");
             float amountOfNumbers = CheckIfValidNumber(Console.ReadLine());
@@ -93,13 +130,12 @@ namespace Calculator
             }
 
             Console.WriteLine("The calculated value of all those numbers is " + calculatedNumber + ".");
-            RestartPrompt();
         }
 
         static void RestartPrompt()
         {
             Console.WriteLine("Type 'Y' if you would like to make another calculation. Otherwise type 'N' if you would like to stop.");
-            if(Console.ReadLine().ToUpper() == "Y")
+            if (Console.ReadLine().ToUpper() == "Y")
             {
                 startCalculation = false;
             }
@@ -110,12 +146,24 @@ namespace Calculator
         {
             float stringToInt;
 
-            while(!float.TryParse(userInput,out stringToInt))
+            while (!float.TryParse(userInput, out stringToInt))
             {
                 Console.WriteLine("Only a number can be entered; please try again.");
                 userInput = Console.ReadLine();
             }
             return float.Parse(userInput);
+        }
+
+        static DateTime CheckIfValidDate(string userInput)
+        {
+            DateTime stringToInt;
+
+            while (!DateTime.TryParse(userInput, out stringToInt))
+            {
+                Console.WriteLine("Please only enter the date in DD/MM/YYYY format.");
+                userInput = Console.ReadLine();
+            }
+            return DateTime.Parse(userInput);
         }
     }
 }
